38fc57e3c2cbe5ce9af28d839654e2dd808a194c
Eugene Koslov <ev.koslov@gmail.com>
2017-02-24 16:00:14 +0200

    Added point position to FrameBody class. This parameters are representing cursor current position

--- common/src/main/java/ev/koslov/remote_control/common/bodies/FrameBody.java
+++ common/src/main/java/ev/koslov/remote_control/common/bodies/FrameBody.java
@@ -6,15 +6,18 @@ import ev.koslov.data_exchanging.components.SerializationUtils;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferInt;
 import java.io.*;
+import java.util.Arrays;
 
 
 public class FrameBody extends ResponseBody {
-    private int frameW, frameH;
+    private int frameW, frameH, pointerX, pointerY;
     private byte[] data;
 
-    public FrameBody(int frameW, int frameH, int[] frameBuffer) throws IOException {
+    public FrameBody(int frameW, int frameH, int pointerX, int pointerY, int[] frameBuffer) throws IOException {
         this.frameW = frameW;
         this.frameH = frameH;
+        this.pointerX = pointerX;
+        this.pointerY = pointerY;
         this.data = SerializationUtils.compress(SerializationUtils.serialize(frameBuffer));
     }
 
@@ -26,6 +29,14 @@ public class FrameBody extends ResponseBody {
         return frameH;
     }
 
+    public int getPointerX() {
+        return pointerX;
+    }
+
+    public int getPointerY() {
+        return pointerY;
+    }
+
     public int[] getFrameBuffer() throws IOException {
         return SerializationUtils.deserialize(SerializationUtils.decompress(data));
     }
@@ -34,13 +45,13 @@ public class FrameBody extends ResponseBody {
         BufferedImage result = new BufferedImage(frameW, frameH, imageType);
         int[] sourceBuffer = getFrameBuffer();
         int[] resultImageBuffer = ((DataBufferInt) result.getRaster().getDataBuffer()).getData();
-        for (int i=0; i<resultImageBuffer.length; i++){
-            resultImageBuffer[i] = sourceBuffer[i];
-        }
+
+        System.arraycopy(sourceBuffer, 0, resultImageBuffer, 0, sourceBuffer.length);
+
         return result;
     }
 
-    public int compressedDataSize(){
+    public int compressedDataSize() {
         return data.length;
     }
 }
